5b4572f98f30055df3390d56c5b2929eae95eda4
Timothé <ottin.timothe@gmail.com>
2022-02-27 18:43:59 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+import { defineNuxtConfig } from "nuxt3";
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  css: ["~/assets/css/main"],
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./assets/**/*.css",
+    "./components/*.{vue,ts}",
+    "./components/**/*.{vue,ts}",
+    "./pages/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./*.{vue,js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
